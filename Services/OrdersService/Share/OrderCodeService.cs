c8b2b7f71606793fb246bafafeae627c92c2c0dc
atikinvobud <dubov.n5@gmail.com>
2025-11-26 11:04:43 +0300

    Added Cancelled Code

--- Services/OrdersService/Share/OrderCodeService.cs
+++ Services/OrdersService/Share/OrderCodeService.cs
@@ -9,6 +9,7 @@ public static class OrderCodeService
     public static Guid GetMessage(OrderStatuses? code) => code switch
     {
         OrderStatuses.InWork => Guid.Parse("9756cd25-f3b3-4e49-a3aa-b20e8072593e"),
+        OrderStatuses.Cancelled => Guid.Parse("241a33f8-0fae-4613-93b7-f2b3a9bbfc04"),
         OrderStatuses.Waiting => Guid.Parse("b6407f50-ef6b-409d-8694-b7057c37da03"),
         OrderStatuses.WaitingApprove => Guid.Parse("821e9bd3-ebd1-44ab-b473-dec2a28b9038"),
         OrderStatuses.Ended => Guid.Parse("5f495a98-ed07-4f46-ada7-a96a9b8ee1dd"),
